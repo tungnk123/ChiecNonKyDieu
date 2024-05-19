6ebaefb3affc53ca7ccc95bd24c45324a897d0fe
Nguyen Hoai <21520238@gm.uit.edu.vn>
2024-05-19 11:48:27 +0700

    Payment API Google Pay

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    viewBinding {
+        enable = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -64,4 +66,32 @@ dependencies {
     // gemini
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.ai.client.generativeai:generativeai:0.4.0")
+
+    // google pay API
+    val lifecycleVersion = "2.7.0"
+
+    implementation("com.google.android.gms:play-services-wallet:19.3.0-beta01")
+    implementation("com.google.pay.button:compose-pay-button:0.1.3")
+
+
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material3:material3")
+
+    val composeBom = platform("androidx.compose:compose-bom:2024.05.00")
+    implementation(composeBom)
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
+
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0")
+
 }
---
+++ app/src/main/java/com/example/chiecnonkydieu/data/CoinPayment.kt
@@ -0,0 +1,7 @@
+package com.example.chiecnonkydieu.data
+
+import android.content.res.Resources
+
+data class CoinPayment(var amountCoin: String, var extraCoin: String,var imageMoney: Int, var moneyPayment: String) {
+
+}
